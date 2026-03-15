b23fa7bb7f2ade097ea9fbb79c5584093ba1dbf7
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-03-15 18:49:06 +0100

    ✨: integrate Renderer class into Experience for improved rendering management

--- src/experience/experience.js
+++ src/experience/experience.js
@@ -2,6 +2,7 @@ import Sizes from "./utils/sizes.js";
 import Time from "./utils/time.js";
 import * as THREE from "three";
 import Camera from "./camera.js";
+import Renderer from "./renderer.js";
 
 let instance = null;
 
@@ -20,6 +21,7 @@ export default class Experience {
     this.time = new Time();
     this.scene = new THREE.Scene();
     this.camera = new Camera();
+    this.renderer = new Renderer();
 
     // Sizes resize event
     this.sizes.on("resize", () => {
@@ -34,9 +36,11 @@ export default class Experience {
 
   resize() {
     this.camera.resize();
+    this.renderer.resize();
   }
 
   update() {
     this.camera.update();
+    this.renderer.update();
   }
 }
