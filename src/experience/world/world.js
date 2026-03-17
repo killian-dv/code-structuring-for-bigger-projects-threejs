3a6fb97b05ac6073e91b19292bd63446e8d268c8
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-03-17 22:47:05 +0100

    ✨: add Fox class for 3D model integration and animation, update World class to include Fox instance and update method

--- src/experience/world/world.js
+++ src/experience/world/world.js
@@ -1,7 +1,7 @@
-import * as THREE from "three";
 import Experience from "../experience.js";
 import Environment from "./environment.js";
 import Floor from "./floor.js";
+import Fox from "./fox.js";
 
 export default class World {
   constructor() {
@@ -9,16 +9,16 @@ export default class World {
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
 
-    // test mesh
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial(),
-    );
-    this.scene.add(testMesh);
-
     this.resources.on("ready", () => {
       this.floor = new Floor();
+      this.fox = new Fox();
       this.environment = new Environment();
     });
+  }
+
+  update() {
+    if (this.fox) {
+      this.fox.update();
+    }
   }
 }
