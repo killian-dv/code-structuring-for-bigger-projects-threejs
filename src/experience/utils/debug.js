12331718b320c10315a5fd6a90aba1eddaa3da6f
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-03-17 23:22:11 +0100

    🔧: set Debug utility to always active for consistent debugging experience

--- src/experience/utils/debug.js
+++ src/experience/utils/debug.js
@@ -2,7 +2,7 @@ import GUI from "lil-gui";
 
 export default class Debug {
   constructor() {
-    this.active = window.location.hash === "#debug";
+    this.active = true;
 
     if (this.active) {
       this.ui = new GUI();
